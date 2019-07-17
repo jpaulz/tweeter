c0470a1cdfb318abcff389e55b8150b48f0176e0
Julia Paulasaari <julia@paulasaari.com>
2019-07-17 01:23:08 +0000

    implemented the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $('.new-tweet textarea').on('keyup', function(event) {
+    let a = $(this).val();
+    $(".counter").text(140 - a.length);
+    if (a.length > 140) {
+      // console.log($(this));
+      $(".counter").css("color", "#ed2a07");
+    } else {
+      $(".counter").css("color", "#545149");
+
+    }
+  });
+});
+
